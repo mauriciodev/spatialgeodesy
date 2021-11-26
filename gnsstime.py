2b4b28fa68dc1dc6e46491a795b8ab2c5acfa6d4
mauriciodev <mauricio.dev@gmail.com>
2021-11-26 14:56:44 -0300

    Fixed two digit years and removed ERFA's depency for JD.

--- gnsstime.py
+++ gnsstime.py
@@ -1,13 +1,20 @@
-import erfa
+#import erfa
 import numpy as np
-from datetime import datetime
 
 def getMJD(t):
     return getJulianDay(t)- 2400000.5
 
 def getJulianDay(t):
-    aux=erfa.cal2jd(getYear(t),getMonth(t),getDay(t))
-    return aux[0]+aux[1]
+    #aux=erfa.cal2jd(getYear(t),getMonth(t),getDay(t))
+    year=getYear(t)
+    month=getMonth(t)
+    day=getDay(t)
+    h=getSecsOfDay(t)/3600.
+    if (month<=2):
+        year=year-1
+        month=month+12
+    JD=np.floor(365.25*year)+np.floor(30.6001*(month+1))+day+1720981.5 +h/24
+    return JD#aux[0]+aux[1]
 
 def getGpsWeek(t):
     gps_t0=np.datetime64('1980-01-06T00:00:00')
@@ -42,8 +49,7 @@ def getDay(t):
     return int((t - t.astype('datetime64[M]'))/ np.timedelta64(1, 'D')) + 1
 
 def getTwoDigitYear(t):
-    year=getYear(t)
-    return year.astype(int)-30
+    return getYear(t)%100
 
 def getDOY(t):
   year=t.astype('datetime64[Y]')
@@ -58,26 +64,13 @@ if __name__=="__main__":
     t=np.datetime64('2021-05-30T23:45:00') 
     
     print("Time: ",t)
-    today=t.astype('datetime64[D]')
-    secsOfDay=(t-today)/ np.timedelta64(1, 's')
-    print("Seconds of day:" , secsOfDay)
-    print(getSecsOfDay(t))
+    print("Seconds of day:" , getSecsOfDay(t))
     #dayOfWeek=(t.astype('datetime64[D]').view('int64') - 3) % 7
-    gps_t0=np.datetime64('1980-01-06T00:00:00')
-    gpsWeek=((t - gps_t0)/ np.timedelta64(1, 'D')/7).astype(np.int64)
-    print("GPS week", gpsWeek)
-    print(getGpsWeek(t))
-    dayOfWeek=((t - gps_t0)/ np.timedelta64(1, 'D') % 7).astype(np.int64)
-    print("Day of GPS week:", dayOfWeek)
-    print(getDayOfWeek(t))
-    year=t.astype('datetime64[Y]')
-    dayOfYear=((t-year)/np.timedelta64(1,'D')+1).astype(np.int64)
-    print("Day of year:",dayOfYear)
-    print(getDayOfYear(t))
-    secsOfWeek=secsOfDay+dayOfWeek*24*60*60
-    print("Seconds of week:",secsOfWeek)
-    twoDigitYear=year.astype(int)-30
-    print("Year with two digits:",twoDigitYear)
+    print("GPS week", getGpsWeek(t))
+    print("Day of GPS week:", getDayOfWeek(t))
+    print("Day of year:",getDayOfYear(t))
+    print("Seconds of week:",getSecsOfWeek(t))
+    print("Year with two digits:",getTwoDigitYear(t))
     print("Day:", getDay(t))
     print("Month:", getMonth(t))
     print("Year:", getYear(t))
